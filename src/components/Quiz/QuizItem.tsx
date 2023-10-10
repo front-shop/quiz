96b6d2b592c17408869a4cec715e05645d875982
mariia.mynkina <mariia.mynkina@danads.se>
2023-10-10 20:07:44 +0300

    Add style for quiz card.

--- src/components/Quiz/QuizItem.tsx
+++ src/components/Quiz/QuizItem.tsx
@@ -4,7 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import {
-  Button, CardActionArea, CardActions
+  Button, CardActionArea, CardActions, styled
 } from '@mui/material';
 import noImage from '../../assets/images/no-image.jpg';
 
@@ -15,16 +15,32 @@ interface IQuizItemProps {
   description?: string,
 }
 
+const CardActionAreaInner = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('sm')]: {
+    heigh: '180px',
+    overflow: 'hidden',
+    '& img': {
+      transition: 'all .8s ease-out',
+    },
+    '&:hover img': {
+      transform: 'scale(1.05)',
+    },
+  },
+
+}));
+
 export default function QuizItem({ item }: { item: IQuizItemProps }) {
   return (
     <Card sx={{ height: '100%' }}>
       <CardActionArea href={`/quiz/${item.id}`}>
-        <CardMedia
-          component="img"
-          height="180"
-          image={item.img || noImage}
-          alt={item.title}
-        />
+        <CardActionAreaInner>
+          <CardMedia
+            component="img"
+            height="180"
+            image={item.img || noImage}
+            alt={item.title}
+          />
+        </CardActionAreaInner>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {item.title}
