c6c701e56568a6fc3bcf32601d550dcfa6ad7c52
mariia.mynkina <mariia.mynkina@danads.se>
2023-10-25 18:44:45 +0300

    Add style.ts and tests for Home page component.

---
+++ src/pages/Home/Home.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Home from './Home';
+import { store } from '../../store/store';
+
+jest.mock('../../components/Quiz/QuizItems', () => function MockInfo() {
+  return <div>Test QuizItems component</div>;
+});
+
+describe('Home Component', () => {
+  const renderHomePage = () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+  };
+
+  it('renders the Search component', () => {
+    renderHomePage();
+    expect(screen.getByRole('heading', { level: 5, name: 'Search bar' })).toBeInTheDocument();
+  });
+
+  it('renders the Quizzes heading and QuizItems', async () => {
+    renderHomePage();
+
+    expect(screen.getByRole('heading', { level: 3, name: 'Quizzes' })).toBeInTheDocument();
+    const quizItems = await screen.findByText('Test QuizItems component');
+    expect(quizItems).toBeInTheDocument();
+  });
+});
